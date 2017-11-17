ae0821aa0502a14b616ac782611fe35227ae3a94
ga32xan <domenik.zimmermann@tum.de>
2017-11-17 16:20:08 +0100

    Added comments to file
    renamed main() to read_gauges()
    serial connection object is now global

--- pressure2.py
+++ pressure2.py
@@ -33,55 +33,74 @@ def bytetoarray(byte):
 '''
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
-def main():
-    #ser.read()
-    #pressure=[]
+def read_gauges():
+    global ser
+    ''' Reads all 6 channels and returns status and (if applicable) pressure '''
+    '''  There is one list for status[CH] and one for pressure[CH] '''
     ser.flushInput()
-    
+
     #send_command('ETX\r\n')
     #print(read_port())
     #send_command('\x05\r\n')
     press=[]
     stat=[]
     for j in range(6):
-        send_command('PR%i\r\n'%(j+1))
-        send_command('\x05')
-        string=read_port().split(',')
-        string_pres=str(string[1])
-        string_sta=int(string[0][-1])
-        press.append(float(string_pres))
-        stat.append(int(string_sta))
+        send_command('PR%i\r\n'%(j+1))  #request Channel 1-6
+        send_command('\x05')            #enquire data
+        #what the controller returns is something like 'x,x.xxxEsx <CR><LF>'
+        #first digit is the error code, seconds one is the pressure
+        '''x,x.xxxEsx <CR><LF>
+        x[Status],[x.xxxEsx] Measurement value (always exponential format)
+        0 Measurement data okay, 1 Underrange, 2 Overrange
+        3 Sensor error, 4 Sensor off, 5 No sensor, 6 Identification erro
+        '''
+        string=read_port().split(',') # splits read string into string[-1],string[0]
+        if debug: print(string)
+        string_pres=str(string[1])       #pressure value converted to string
+        if debug: print('Read pressure :' + string_pres)
+        string_sta=int(string[0][-1])    #status value converted to int
+        if debug: print('Read status :' + str(string_stat))
+        press.append(float(string_pres))    #append float of pressure to press-list
+        stat.append(int(string_stat))        #append int(status) to status list
     return(stat,press)
     
 def send_command(command):
-    if debug: print('########################')
-    input=command.encode('utf-8')
-    if debug: print('Command string: ' + str(input))
-    convinput=to_bytes(input)
-    if debug: print('byte-input (as str repre): ' + str(convinput.decode('utf-8')))
-    if debug: print('CTS line: ' + str(ser.cts))
-    if debug: print('DSR line: ' + str(ser.dsr))
-    ser.write(convinput)
+    global ser 
+    '''Takes ascii string 'command' and converts it to bytes and sned it over serial connection '''
+    if debug2: print('########################')
+    input=command.encode('utf-8')   #encode as utf-8
+    if debug2: print('Command string: ' + str(input))
+    convinput=to_bytes(input)       #convert to byte sequence
+    if debug2: print('byte-input (as str repre): ' + str(convinput.decode('utf-8')))
+    if debug2: print('CTS line: ' + str(ser.cts))
+    if debug2: print('DSR line: ' + str(ser.dsr))
+    ser.write(convinput)            #send to wire
     time.sleep(0.2)
-    if debug: print('########################')
-    if debug: print('Send Command: ' + str(input))
+    if debug2: print('########################')
+    if debug2: print('Send Command: ' + str(input))
     
 def read_port():
-    
+    ''' Reads serial port, gets bytes over wire, decodes them with utf-8'''
+    ''' and returns string with received message'''
     if debug: print('########################')
-    if debug: print('Am I outWaiting?: ' + str(ser.out_waiting))
+    if debug2: print('Am I outWaiting?: ' + str(ser.out_waiting))
     if debug: print('Am I inWaiting?: ' + str(ser.in_waiting))
+    if debug: print('Input buffer size: ' + str(ser.in_waiting))
     if debug: print('########################')
-    if debug: print('...ser.read ...')
-    if debug: print('CTS line: ' + str(ser.cts))
-    if debug: print('DSR line: ' + str(ser.dsr))
+    if debug2: print('CTS line: ' + str(ser.cts))
+    if debug2: print('DSR line: ' + str(ser.dsr))
     
-    out = ''
-    input_buffersize = ser.in_waiting
+    out = ''#string to hold the received message, empty one for new reading
+    input_buffersize = ser.in_waiting   #input_buffersize: Numbers of bytes received
+    if debug:
+        if input_buffersize == 0:
+            print('No data in input buffer...No data received')
     while input_buffersize > 0:
+        '''  '''
+        if debug: print('Input buffersize: ' + str(input_buffersize))
+        if debug: print('...ser.read ...')
         input_buffersize_old = 0
         time.sleep(0.2)
-        if debug: print('Input buffersize: ' + str(input_buffersize))
         out += ser.read(64).decode('utf-8')
         if debug: print('accomplished')
         if input_buffersize == input_buffersize_old:
@@ -93,6 +112,7 @@ def read_port():
     return out
 
 def get_info():
+    global ser
     print('############ Information about connection: ############')
     print('Name of device: ' + ser.name)
     print('@ port : ' + ser.port)
@@ -118,118 +138,130 @@ def get_info():
     print('\t \t \t \t... DSR line: ' + str(ser.dsrdtr))
     print('RS485 settings: ' +  str(ser.rs485_mode))
 
+def init_serial():
+    global ser       #create global serial-connector object
+    try:
+        ser = serial.Serial(port='COM1',timeout=0.5,baudrate=9600,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE)    
+    except IndexError as err:
+        print('Failed opening serial port...Try reloading the Console')
+    if debug2: get_info()
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+
 if __name__ == '__main__':
-	ser = serial.Serial(port='/dev/ttyUSB0',timeout=0.5,baudrate=9600,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE)    
-	debug = 0
-	ser.reset_input_buffer()
-	ser.reset_output_buffer()
-	pressures=[[],[],[],[],[],[]]
-	times=[]
-	col=['b','r','g','K','c','y']
-	
-	labels_begin=[r'STM',r'Rough',r'Prep',r'Sensor 4',r'Sensor 5',r'Sensor 6']
-	
-	fig=plt.figure(figsize=(10,6),dpi=100)
-	ax=fig.add_subplot(111)
-	date_fmt = '%d-%m-%Y %H:%M:%S'
-	plt.ion()
-	plt.yscale('log')
-	
-	stat,stpre=main()
-	
-	labels=['','','','','','']
-	
-	for num,sensor in enumerate(stat):
-		if sensor==0:
-			pressures[num].append(stpre[num])
-			if pressures[num][-1]>1e-1:
-				labels[num]=labels_begin[num]+r' $\rightarrow$'
-			elif pressures[num][-1]<1e-1:
-				labels[num]=labels_begin[num]+r' $\leftarrow$'
-		elif sensor==1:
-			pressures[num].append(0)
-			labels[num]=labels_begin[num]+' - Underrange'
-		elif sensor==2:
-			pressures[num].append(0)
-			labels[num]=labels_begin[num]+' - Overrange'
-		elif sensor==3:
-			pressures[num].append(0)
-			labels[num]=labels_begin[num]+' - Error'
-		elif sensor==4:
-			pressures[num].append(0)
-			labels[num]=labels_begin[num]+' - Off'
-		elif sensor==5:
-			pressures[num].append(0)
-			labels[num]=labels_begin[num]+' - Not found'
-		elif sensor==6:
-			pressures[num].append(0)
-			labels[num]=labels_begin[num]+' - Identification error'
-	datenow=dt.datetime.now().strftime(date_fmt)
-	times.append(mdate.datestr2num(datenow))
-	header = 'Time\t\t\t\t\tSTM [mbar]\t\tRough [mbar]\tPrep [mbar]\t\tSensor 4 [mbar]\t\tSensor 5 [mbar]\t\tSensor 6 [mbar]\n'
-	if os.path.isfile('pressure-log.txt'):
-		header = '##########################################################################'
-	with open("pressure-log.txt", "a") as myfile:
-		myfile.write(header)
-		myfile.write("%s\t\t%.2e\t\t%.2e\t\t%.2e\t\t%.2e\t\t\t%.2e\t\t\t%.2e\n"%(datenow,pressures[0][0],pressures[1][0],pressures[2][0],pressures[3][0],pressures[4][0],pressures[5][0]))
-	sens={}
-	for j in range(6):
-		sens['sen{0}'.format(j)],=ax.plot(times,pressures[j],'o',ls='-',color=col[j],label=labels[j])
-	ax.set_ylim(1e-12,1e-4)
-	ax.set_xlabel('Time')
-	ax.set_ylabel('Pressure [mbar]')
-	
-	ax.legend()
-	plt.gca().xaxis.set_major_formatter(mdate.DateFormatter(date_fmt))
-	plt.gcf().autofmt_xdate()
-	
-	ax2 = ax.twinx()
-	for j in range(6):
-		if pressures[j][-1]>1e-1:
-			sens['sen{0}'.format(j)],=ax2.plot(times,pressures[j],'o',ls='-',color=col[j],label=labels[j])
-	ax2.set_ylim(1e-1,1e3)
-	ax2.set_yscale('log')
-	ax2.set_ylabel('Pressure [mbar]')
-	
-	while True:
-		ser.open()
-		status,pre=main()
-		datenow=dt.datetime.now().strftime(date_fmt)
-		times.append(mdate.datestr2num(datenow))
-		ax.legend_.remove()
-		for num,sensor in enumerate(status):
-			if sensor==0:
-				pressures[num].append(pre[num])
-				if pressures[num][-1]>1e-1:
-					labels[num]=labels_begin[num]+r' $\rightarrow$'
-				elif pressures[num][-1]<1e-1:
-					labels[num]=labels_begin[num]+r' $\leftarrow$'
-			elif sensor==1:
-				pressures[num].append(0)
-				labels[num]=labels_begin[num]+' - Underrange'
-			elif sensor==2:
-				pressures[num].append(0)
-				labels[num]=labels_begin[num]+' - Overrange'
-			elif sensor==3:
-				pressures[num].append(0)
-				labels[num]=labels_begin[num]+' - Error'
-			elif sensor==4:
-				pressures[num].append(0)
-				labels[num]=labels_begin[num]+' - Off'
-			elif sensor==5:
-				pressures[num].append(0)
-				labels[num]=labels_begin[num]+' - Not found'
-			elif sensor==6:
-				pressures[num].append(0)
-				labels[num]=labels_begin[num]+' - Identification error'
-		with open("pressure.txt", "a") as myfile:
-			myfile.write("%s\t\t%.2e\t\t%.2e\t\t%.2e\t\t%.2e\t\t\t%.2e\t\t\t%.2e\n"%(datenow,pressures[0][-1],pressures[0][-1],pressures[2][-1],pressures[3][-1],pressures[4][-1],pressures[5][-1]))
-		
-		for j in range(6):
-			sens['sen{0}'.format(j)].set_xdata(times)
-			sens['sen{0}'.format(j)].set_ydata(pressures[j])
-			sens['sen{0}'.format(j)].set_label(labels[j])
-		ax.legend()
-		ax.set_xlim(times[0]-(times[1]-times[0]),times[-1]+(times[1]-times[0]))
-		plt.pause(0.05)
-		ser.close()
+    global ser
+    debug = True
+    debug2 = False
+    init_serial()
+    
+    pressures=[[],[],[],[],[],[]]
+    times=[]
+    col=['b','r','g','K','c','y']
+    
+    labels_begin=[r'STM',r'Rough',r'Prep',r'Sensor 4',r'Sensor 5',r'Sensor 6']
+    
+    fig=plt.figure(figsize=(10,6),dpi=100)
+    ax=fig.add_subplot(111)
+    plt.ion()
+    plt.yscale('log')
+    
+    stat,stpre=read_gauges()
+    
+    labels=['','','','','','']
+    
+    for num,sensor in enumerate(stat):
+        if sensor==0:
+            pressures[num].append(stpre[num])
+            if pressures[num][-1]>1e-1:
+                labels[num]=labels_begin[num]+r' $\rightarrow$'
+            elif pressures[num][-1]<1e-1:
+                labels[num]=labels_begin[num]+r' $\leftarrow$'
+        elif sensor==1:
+            pressures[num].append(0)
+            labels[num]=labels_begin[num]+' - Underrange'
+        elif sensor==2:
+            pressures[num].append(0)
+            labels[num]=labels_begin[num]+' - Overrange'
+        elif sensor==3:
+            pressures[num].append(0)
+            labels[num]=labels_begin[num]+' - Error'
+        elif sensor==4:
+            pressures[num].append(0)
+            labels[num]=labels_begin[num]+' - Off'
+        elif sensor==5:
+            pressures[num].append(0)
+            labels[num]=labels_begin[num]+' - Not found'
+        elif sensor==6:
+            pressures[num].append(0)
+            labels[num]=labels_begin[num]+' - Identification error'
+    date_fmt = '%d-%m-%Y %H:%M:%S'
+    datenow=dt.datetime.now().strftime(date_fmt)    # get formatted datetime object
+    times.append(mdate.datestr2num(datenow))        #and append it to times list
+    #write header if logfile was never used ...
+    header = 'Time\t\t\t\t\tSTM [mbar]\t\tRough [mbar]\tPrep [mbar]\t\tSensor 4 [mbar]\t\tSensor 5 [mbar]\t\tSensor 6 [mbar]\n'
+    if os.path.isfile('pressure-log.txt'):  #... or add seperator if logfile was already used to keep old data
+        header = '##########################################################################'
+    with open("pressure-log.txt", "a") as logfile:
+        logfile.write(header)
+        log.write("%s\t\t%.2e\t\t%.2e\t\t%.2e\t\t%.2e\t\t\t%.2e\t\t\t%.2e\n"%(datenow,pressures[0][0],pressures[1][0],pressures[2][0],pressures[3][0],pressures[4][0],pressures[5][0]))
+    sens={}
+    for j in range(6):
+        sens['sen{0}'.format(j)],=ax.plot(times,pressures[j],'o',ls='-',color=col[j],label=labels[j])
+    ax.set_ylim(1e-12,1e-4)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Pressure [mbar]')
+    
+    ax.legend()
+    plt.gca().xaxis.set_major_formatter(mdate.DateFormatter(date_fmt))
+    plt.gcf().autofmt_xdate()
+    
+    ax2 = ax.twinx()
+    for j in range(6):
+        if pressures[j][-1]>1e-1:
+            sens['sen{0}'.format(j)],=ax2.plot(times,pressures[j],'o',ls='-',color=col[j],label=labels[j])
+    ax2.set_ylim(1e-1,1e3)
+    ax2.set_yscale('log')
+    ax2.set_ylabel('Pressure [mbar]')
+    
+    while True:
+        ser.open()
+        status,pre=read_gauges()
+        datenow=dt.datetime.now().strftime(date_fmt)
+        times.append(mdate.datestr2num(datenow))
+        ax.legend_.remove()
+        for num,sensor in enumerate(status):
+            if sensor==0:
+                pressures[num].append(pre[num])
+                if pressures[num][-1]>1e-1:
+                    labels[num]=labels_begin[num]+r' $\rightarrow$'
+                elif pressures[num][-1]<1e-1:
+                    labels[num]=labels_begin[num]+r' $\leftarrow$'
+            elif sensor==1:
+                pressures[num].append(0)
+                labels[num]=labels_begin[num]+' - Underrange'
+            elif sensor==2:
+                pressures[num].append(0)
+                labels[num]=labels_begin[num]+' - Overrange'
+            elif sensor==3:
+                pressures[num].append(0)
+                labels[num]=labels_begin[num]+' - Error'
+            elif sensor==4:
+                pressures[num].append(0)
+                labels[num]=labels_begin[num]+' - Off'
+            elif sensor==5:
+                pressures[num].append(0)
+                labels[num]=labels_begin[num]+' - Not found'
+            elif sensor==6:
+                pressures[num].append(0)
+                labels[num]=labels_begin[num]+' - Identification error'
+        with open("pressure.txt", "a") as logfile:
+            logfile.write("%s\t\t%.2e\t\t%.2e\t\t%.2e\t\t%.2e\t\t\t%.2e\t\t\t%.2e\n"%(datenow,pressures[0][-1],pressures[0][-1],pressures[2][-1],pressures[3][-1],pressures[4][-1],pressures[5][-1]))
+        
+        for j in range(6):
+            sens['sen{0}'.format(j)].set_xdata(times)
+            sens['sen{0}'.format(j)].set_ydata(pressures[j])
+            sens['sen{0}'.format(j)].set_label(labels[j])
+        ax.legend()
+        ax.set_xlim(times[0]-(times[1]-times[0]),times[-1]+(times[1]-times[0]))
+        plt.pause(0.05)
+        ser.close()
