ac43c631d69689a7f220abb579c6ac97f355d8ff
ga32xan <domenik.zimmermann@tum.de>
2017-11-24 14:13:30 +0000

    argparse+log
    adding argparse+logging capabilities

--- Pressure-reading.py
+++ Pressure-reading.py
@@ -17,44 +17,49 @@ import numpy as np
 import os
 import argparse # for the argument parsing stuff and -h explanation
 import logging	# for logging puposes
-##########################################################################################################
+################################################################################
 ''' Create helper when script is used wrong! '''
 ''' Parses arguments given in command line '''
 parser = argparse.ArgumentParser(
-            description = 'Reads pressures from Pfeiffer Maxigauge TPG256A and shows interactive graph if wanted',\
-            epilog = 'For detailed documentation see source code or visit http://github.com/ga32xan/Maxigauge-TPG256A'\
+            description = 'Reads pressures from Pfeiffer Maxigauge TPG256A and \
+			shows interactive graph if wanted',\
+            epilog = 'For detailed documentation see source code or visit \
+			http://github.com/ga32xan/Maxigauge-TPG256A'\
             )
-parser.add_argument('--loglevel',\
-                    help = 'Minimum numeric loglevel/serverity: Debug, Info, Warning, Error, Critical.\
-                    This switch does not affect the pressure log, wchich is always written in the same format.\
+parser.add_argument('--loglevel', '-v',\
+                    help = 'Minimum numeric loglevel/serverity: \n \
+					Debug, Info, Warning, Error, Critical.\n \
+                    This switch does not affect the pressure log, \
+					which is always written in the same detail.\
                     Defaults to Warning.',\
                     type = str,\
                     default = 'Warning'\
                    )
 
-parser.add_argument('--programlogfile',\
-                    help = 'What filename should the pressure log have? Always written to the directory where script is located\
+parser.add_argument('--programlogfile', '-pl',\
+                    help = 'What filename should the pressure log have? \
+					Always written to the directory where script is located\
                     Defaults to control.log',\
                     type = str,\
                     default = 'control.log'\
                    )
 
-parser.add_argument('--pressurelogfile',\
-                    help = 'What filename should the pressure log have? Always written to the directory where script is located\
+parser.add_argument('--pressurelogfile', '-l',\
+                    help = 'What filename should the pressure log have? Always \
+					written to the directory where script is located\
                     Defaults to pressure.log',
                     type = str,\
                     default = 'pressure.log'\
                    )
 
-parser.add_argument('--plot',\
-                    help = 'Plot graph? Do not use outside spyder, yet!'\
+parser.add_argument('--plot', '-p',\
+                    help = 'Plot graph? Do not use outside spyder, yet!\
                     Defaults to False.',
-                    type = bool,\
                     action = 'store_true',\
-                    default = False\
+                    #default = False\ #can be omitted since its the default behaviour
                    )
 
-parser.add_argument('--comport',\
+parser.add_argument('--comport', '-c',\
                     help = 'What port to use? Defaults to 5.',
                     type = int,\
                     default = 5\
@@ -62,15 +67,16 @@ parser.add_argument('--comport',\
 
 arguments = parser.parse_args()
 
-''' Takes argument loglevel and programlogfile from the argparser and passes it to the logging facility '''
+''' Takes argument loglevel and programlogfile from the argparser \
+and passes it to the logging facility '''
 loglevel = arguments.loglevel
 numeric_loglevel = getattr(logging, loglevel.upper(), None)
-if not isinstance(numeric_loglevel, int)
+if not isinstance(numeric_loglevel, int):
     raise ValueError('Invalid log level: %s' % loglevel)
 
 programlogfile_name = os.getcwd() + arguments.programlogfile
 logging.basicConfig(filename = programlogfile_name,\
-                    format = /'(%asctime)s %(message)s',\
+                    format = '(%asctime)s %(message)s',\
                     datefmt = '%d-%m-%Y %H:%M:%S',\
                     filemode = 'w',\
                     level = numeric_loglevel\
@@ -83,10 +89,11 @@ com_port = arguments.comport
 
 ''' Takes argument plot from the argparser '''
 plot = arguments.plot
-##########################################################################################################
+################################################################################
 def read_gauges(ser):
     ''' Reads all 6 channels and returns status and (if applicable) pressure '''
     '''  There is one list for status[CH] called stat and one for pressure[CH] called press returned'''
+    logging.debug('##########read_gauges##############')
     ser.flushInput()
 
     press = []
@@ -102,73 +109,74 @@ def read_gauges(ser):
         0 Measurement data okay, 1 Underrange, 2 Overrange
         3 Sensor error, 4 Sensor off, 5 No sensor, 6 Identification error
         '''
+        logging.debug('##########splitting received pressure string##############')
         string=read_port(ser).split(',') 		# splits read string into string[-1],string[0]
-        if debug: print(string)
+        loggin.debug(string)
         string_pres=str(string[1])       	#pressure value converted to string
-        if debug: print('Read pressure :' + string_pres)
+        logging.debug('Read pressure :' + string_pres)
         string_sta=int(string[0][-1])    	#status value converted to int
-        if debug: print('Read status :' + str(string_sta))
+        logging.debug('Read status :' + str(string_sta))
         press.append(float(string_pres))    	#append float of pressure to press-list
         stat.append(int(string_sta))        	#append int(status) to status list
     return(stat,press)
-##########################################################################################################    
+################################################################################
 def send_command(ser,command):
     ''' Takes ascii string 'command' and converts it to bytes to send it over serial connection '''
-    if debug2: print('########################')
+    logging.debug('##########ser_command##############')
     input = command.encode('utf-8')   #encode as utf-8
-    if debug2: print('Command string: ' + str(input))
+    logging.debug('Command string: ' + str(input))
     convinput=to_bytes(input)       #convert to byte sequence
-    if debug2: print('byte-input (as str repre): ' + str(convinput.decode('utf-8')))
-    if debug2: print('CTS line: ' + str(ser.cts))
-    if debug2: print('DSR line: ' + str(ser.dsr))
+    logging.debug('byte-input (as str repre): ' + str(convinput.decode('utf-8')))
+    logging.debug('CTS line: ' + str(ser.cts))
+    logging.debug('DSR line: ' + str(ser.dsr))
     ser.write(convinput)            #send to wire
     time.sleep(0.05)
-    if debug2: print('########################')
-    if debug2: print('Send Command: ' + str(input))
-##########################################################################################################    
+    logging.debug('Send Command: ' + str(input))
+################################################################################
 def read_port(ser):
     ''' Reads serial port, gets bytes over wire, decodes them with utf-8'''
     ''' and returns string with received message'''
-    if debug:  print('########################')
-    if debug2: print('Am I outWaiting?: ' + str(ser.out_waiting))
-    if debug:  print('Am I inWaiting?: ' + str(ser.in_waiting))
-    if debug:  print('Input buffer size: ' + str(ser.in_waiting))
-    if debug:  print('########################')
-    if debug2: print('CTS line: ' + str(ser.cts))
-    if debug2: print('DSR line: ' + str(ser.dsr))
-    
+    logging.debug('##########read_port##############')
+    logging.debug('Am I outWaiting?: ' + str(ser.out_waiting))
+    logging.debug('Am I inWaiting?: ' + str(ser.in_waiting))
+    logging.debug('Input buffer size: ' + str(ser.in_waiting))
+    logging.debug('CTS line: ' + str(ser.cts))
+    logging.debug('DSR line: ' + str(ser.dsr))
     out = ''                            #string to hold the received message, empty one for new reading
     input_buffersize = ser.in_waiting   #input_buffersize: Numbers of bytes received
-    if debug:
-        if input_buffersize == 0:
-            print('No data in input buffer...No data received')
+    if input_buffersize == 0:
+        logging.warning('No data in input buffer...No data received')
     while input_buffersize > 0:
         ''' runs through twice to check consistency of received message '''
         ''' if first read msg matches snd read msg the input is believed to be consistend '''
         ''' No errror handling => program breaks at this point if no meaningfull serial connection is established '''
-        if debug: print('Input buffersize: ' + str(input_buffersize))
-        if debug: print('...ser.read ...')
+        logging.debug('Input buffersize: ' + str(input_buffersize))
+        logging.debug('...ser.read ...')
         input_buffersize_old = 0
         time.sleep(0.05)
         out += ser.read(64).decode('utf-8')
-        if debug: print('accomplished')
+        logging.debug('accomplished')
         if input_buffersize == input_buffersize_old:
-            if debug: print('Received msg: ' + str(out))
+            logging.debug('Received msg: ' + str(out))
             break
         else:
             input_buffersize = input_buffersize_old
     return out
-##########################################################################################################
+    logging.debug('Received msg: ' + str(out))
+################################################################################
 def test_connection(ser):
+    logging.debug('##########test_connection##############')
     ''' Unimplemented testing routine to test the serial connection object passed as ser '''
     send_command(ser,'PR%i\r\n'%(j+1))  #request Channel 1-6
     send_command(ser,'\x05')            #enquire data
     read_port(ser)
     if True:
         ''' !Some Check routine missing! '''
-##########################################################################################################    
+################################################################################
 def get_info(ser):
     ''' Get information about the serial connection, prints only if debug2 = True '''
+    logging.debug('##########get_info##############')
+    logging.debug('Printing information about connection to console')
     print('############ Information about connection: ############')
     print('Name of device: ' + ser.name)
     print('@ port : ' + ser.port)
@@ -193,9 +201,10 @@ def get_info(ser):
     print('\t \t \t \t... CTS line: ' + str(ser.rtscts))
     print('\t \t \t \t... DSR line: ' + str(ser.dsrdtr))
     print('RS485 settings: ' +  str(ser.rs485_mode))
-##########################################################################################################
+################################################################################
 def init_serial(com_port = 5):
     ''' Initializes serial connection, defaults to COM5 '''
+    logging.debug('##########init_serial##############')
     try:
         ser = serial.Serial(timeout=0.5,\
 			    baudrate=9600,\
@@ -207,13 +216,17 @@ def init_serial(com_port = 5):
         ser.open()
         ser.reset_input_buffer()
         ser.reset_output_buffer()
-        if debug2: get_info(ser)
+        if numeric_loglevel < 3: get_info(ser)
+        logging.debug('init_serial on COM' + str(com_port) + 'succesfully')
         return ser
     except IndexError as err:
+        logging.critical('Opening of com-port failed')
         print('Failed opening serial port at port' + str(ser.port))
         print('Make sure you are on the right COM port and try reloading the console')
-##########################################################################################################
+################################################################################
 def to_bytes(seq):
+    logging.debug('##########to_serial##############')
+
     """convert a sequence of int/str to a byte sequence and returns it"""
     if isinstance(seq, bytes):
         return seq
@@ -226,16 +239,18 @@ def to_bytes(seq):
         for item in seq:
             b.append(item)  # this one handles int and str for our emulation and ints for Python 3.x
         return bytes(b)
-########################################## Main routine ################################################    
+        logging.debug('Byte-conversion for ' + str(seq) + ' done')
+########################################## Main routine ########################
 if __name__ == '__main__':
     ''' Messy routine that updates the data, plot it and updates the logfile '''
-    ''' Every time the program is started and writes to the same logfile a line of '#############' is added '''
+    ''' Every time the program is started and writes to the same logfile a line of # is added '''
     ''' TODO: cleanup, write subroutine update_plot and write_logfile '''
-    if arguments.log == 0: debug = False          # First debug level   
-    if arguments.log == 1: debug = True           # First debug level   
-    if arguments.log > 1: 
-        debug = True           # First debug level   
-        debug2 = True          # extensive debugging+
+    logging.debug('##########main()##############')
+    logging.debug(arguments)
+    logging.debug('First Bebug-msg')
+    logging.info('First info-msg')
+    logging.warning('First warning-msg')
+    logging.critical('First critical-msg')
     ''' Boolean to indicate if auto-updating matplotlib-graph is wanted gives Priviledge Error if not executed within '''
     ''' Set to False if script is exucuted from command line '''
     ''' Set to true if run in IDE (tested: Anaconda) '''
@@ -261,8 +276,11 @@ if __name__ == '__main__':
     labels = ['','','','','','']
     labels_begin = [r'STM',r'Rough',r'Prep',r'Sensor 4',r'Sensor 5',r'Sensor 6']
     for sensor_num,status in enumerate(stat):
+        logging.debug('##########updating pressures inside main()##############')
+        logging.debug('Sensor: ' + str(sensor_num))
         ''' enumerate(stat) returns 0,stat[0] ... 1,stat[1] ... 2,stat[2] ... '''
         if status == 0:
+            logging.debug('Channel OK')
             pressures[sensor_num].append(stpre[sensor_num])
             if plot:
                 if pressures[sensor_num][-1] > 1e-1:
@@ -270,24 +288,29 @@ if __name__ == '__main__':
                 elif pressures[sensor_num][-1] <= 1e-1:
                     labels[sensor_num] = labels_begin[sensor_num]+r' $\leftarrow$ %.2f mbar'%pressures[sensor_num][-1]
         elif status == 1:
+            logging.warning('Channel Underrange')
             pressures[sensor_num].append(1e10)
             if plot: labels[sensor_num] = labels_begin[sensor_num]+' - Underrange'
         elif status == 2:
+            logging.warning('Channel Overrange')
             pressures[sensor_num].append(1e10)
             if plot:  labels[sensor_num] = labels_begin[sensor_num]+' - Overrange'
         elif status == 3:
+            logging.error('Channel Error')
             pressures[sensor_num].append(1e10)
             if plot:  labels[sensor_num] = labels_begin[sensor_num]+' - Error'
         elif status == 4:
+            logging.debug('Channel Off')
             pressures[sensor_num].append(1e10)
             if plot:  labels[sensor_num] = labels_begin[sensor_num]+' - Off'
         elif status == 5:
+            logging.debug('Channel Not found')
             pressures[sensor_num].append(1e10)
             if plot:  labels[sensor_num] = labels_begin[sensor_num]+' - Not found'
         elif status == 6:
+            logging.error('Channel Identification Error')
             pressures[sensor_num].append(1e10)
             if plot: labels[sensor_num] = labels_begin[sensor_num]+' - Identification error'
-    
     ''' Prepares and writes logfile '''  
     date_fmt = '%d-%m-%Y %H:%M:%S'
     datenow = dt.datetime.now().strftime(date_fmt)      # get formatted datetime object
@@ -299,10 +322,11 @@ if __name__ == '__main__':
         header = '##################### Program restarted ###################################\n'
     with open(pressurelogfile_name, "a") as logfile:
         logfile.write(header)
-        logfile.write("%s\t\t%.2e\t\t%.2e\t\t%.2e\t\t%.2e\t\t\t%.2e\t\t\t%.2e\n"%(datenow,pressures[0][0],pressures[1][0],pressures[2][0],pressures[3][0],pressures[4][0],pressures[5][0]))
-    
+        logfile.write("%s\t\t%.2e\t\t%.2e\t\t%.2e\t\t%.2e\t\t\t%.2e\t\t\t%.2e\n"\
+		%(datenow,pressures[0][0],pressures[1][0],pressures[2][0],pressures[3][0],pressures[4][0],pressures[5][0]))    
     ''' Prepare plot '''
     if plot:
+        logging.debug('Preparing plot')
         fig = plt.figure(figsize=(10,6),dpi=100)
         ax = fig.add_subplot(111)
         plt.ion()                      #autoupdate plot
@@ -333,6 +357,7 @@ if __name__ == '__main__':
         ax2.set_yscale('log')
         ax2.set_ylabel('Pressure [mbar]')
     while True:
+        logging.debug('Loop-Top')
         ''' Keep Com port open for only a short amount of time so that if the program is killed it is most likely in a closed state '''
         ''' This should be done via a try: except: statement to make it exit nicely '''
         
@@ -394,4 +419,4 @@ if __name__ == '__main__':
             ax.set_xlim(times[0]-(times[1]-times[0]),times[-1]+(times[1]-times[0]))
             #asis range: 12h
             #ax.set_xlim(dt.datetime.now()-dt.timedelta(hours=12),times[-1]+(times[1]-times[0]))
-    plt.pause(1)
+            plt.pause(1)
